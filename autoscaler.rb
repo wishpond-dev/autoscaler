8672a092882854eea4889a743fcf78428a1b11ef
Morgan Grubb <morgan@wishpond.com>
2020-07-22 12:15:44 -0700

    Refactorify

--- autoscaler.rb
+++ autoscaler.rb
@@ -1,71 +1,123 @@
-selector = ENV['SELECTOR'] || raise("SELECTOR required")
-resource = ENV['RESOURCE'] || "deployment"
-name = ENV['NAME'] || "#{resource}/#{selector}"
-max_pods = (ENV['MAX_PODS'] || '10').to_i
-min_pods = (ENV['MIN_PODS'] || '1').to_i
-strategy = ENV['STRATEGY'] || 'average_cpu'
+class Autoscaler
+  POD_DATA_FILE = '/tmp/pods'
 
-# Get the list of pods
-#
-# TODO: Filter out any pods that aren't relevant
-#
-command = %(kubectl top pods -l #{selector} --no-headers > /tmp/pods)
-`#{command}`
-
-pods = `cat /tmp/pods | wc -l`.chomp.to_i
-cpu = `awk '{s+=$2} END {print s}' /tmp/pods`.chomp.to_i
-utilisation = cpu / pods
-new_pods = pods
-direction = 'none'
-
-puts "name:#{name} selector:#{selector} cpu:#{cpu} pods:#{pods} utilisation:#{utilisation} strategy:#{strategy}"
-
-case strategy
-when 'average_cpu'
-  scale_up = (ENV['SCALE_UP'] || '500').to_i
-  scale_down = (ENV['SCALE_DOWN'] || '100').to_i
-  factor = (ENV['FACTOR'] || '2').to_i
-
-  if utilisation > scale_up
-    new_pods = pods * factor
-    new_pods = [new_pods, max_pods].min
-    direction = 'up'
-  elsif utilisation < scale_down
-    new_pods = pods / factor
-    new_pods = [new_pods, min_pods].max
-    direction = 'down'
-  end
-when 'metric'
-  current_value = %x(curl -s #{ENV['ENDPOINT']}).to_f
-  desired_value = (ENV['TARGET'] || '1').to_f
-
-  # Desired value can't be zero
-  raise "TARGET can't be 0" if desired_value == '0'.to_f
-
-  new_pods = (pods * (current_value / desired_value)).ceil
-
-  if new_pods > pods
-    new_pods = [new_pods, max_pods].min
-    direction = 'up'
-  elsif new_pods < pods
-    new_pods = [new_pods, min_pods].max
-    direction = 'down'
+  class << self
+    def run
+      new.run
+    end
   end
 
-  puts "current:#{current_value} desired:#{desired_value} new_pods:#{new_pods} direction:#{direction}"
+  def run
+    log "name:#{name} selector:#{selector} cpu:#{cpu} pods:#{pods} utilisation:#{utilisation} strategy:#{strategy}"
 
-else
-  raise "Unknown strategy: #{strategy}"
+    if new_pods != pods
+      log "scaling #{direction}"
+      log %x(kubectl scale --replicas #{new_pods} #{resource} -l #{selector})
+      `curl -X POST https://katana.wishpond.com/notify/scale \
+            --header "Content-Type: application/json" \
+            --header "X-KATANA-TOKEN:#{ENV['KATANA_SECRET']}" \
+            -d "{\"name\":\"#{name}\",\"selector\":\"#{selector}\",\"cpu\":\"#{cpu}\",\"pods\":\"#{pods}\",\"new_pods\":\"#{new_pods}\",\"utilisation\":\"#{utilisation}\",\"resource\":\"#{resource}\",\"direction\":\"#{direction}\"}"`
+    else
+      log "no change"
+    end
+  end
+
+  def log(message)
+    puts "#{Time.now} #{message}"
+  end
+
+  def pods
+    get_current_state!
+    @pods ||= `cat #{POD_DATA_FILE} | wc -l`.chomp.to_i
+  end
+
+  def cpu
+    get_current_state!
+    @cpu ||= `awk '{s+=$2} END {print s}' #{POD_DATA_FILE}`.chomp.to_i
+  end
+
+  def utilisation
+    @utilisation ||= cpu / pods
+  end
+
+  def get_current_state!
+    return if @state_acquired
+    @state_acquired = true
+    `kubectl top pods -l #{selector} --no-headers > #{POD_DATA_FILE}`
+  end
+
+  def strategy
+    @strategy ||= ENV['STRATEGY'] || 'average_cpu'
+  end
+
+  def min_pods
+    @min_pods ||= (ENV['MIN_PODS'] || '1').to_i
+  end
+
+  def max_pods
+    @max_pods ||= (ENV['MAX_PODS'] || '10').to_i
+  end
+
+  def name
+    @name ||= ENV['NAME'] || "#{resource}/#{selector}"
+  end
+
+  def resource
+    @resource ||= ENV['RESOURCE'] || "deployment"
+  end
+
+  def selector
+    @selector ||= ENV['SELECTOR'] || raise("SELECTOR required")
+  end
+
+  def new_pods
+    @new_pods ||= [[desired_pods, min_pods].max, max_pods].min
+  end
+
+  def direction
+    @direction =
+      if new_pods > pods
+        'up'
+      elsif new_pods < pods
+        'down'
+      else
+        ''
+      end
+  end
+
+  def desired_pods
+    @desired_pods ||=
+      case strategy
+      when 'average_cpu'
+        new_pods_by_average_cpu
+      when 'metric'
+        new_pods_by_metric
+      else
+        raise "Unknown strategy: #{strategy}"
+      end
+  end
+
+  def new_pods_by_average_cpu
+    scale_up = (ENV['SCALE_UP'] || '500').to_i
+    scale_down = (ENV['SCALE_DOWN'] || '100').to_i
+    factor = (ENV['FACTOR'] || '2').to_i
+
+    if utilisation > scale_up
+      pods * factor
+    elsif utilisation < scale_down
+      pods / factor
+    end
+  end
+
+  def new_pods_by_metric
+    current_value = %x(curl -s #{ENV['ENDPOINT']}).to_f
+    desired_value = (ENV['TARGET'] || '1').to_f
+
+    # Desired value can't be zero
+    raise "TARGET can't be 0" if desired_value == '0'.to_f
+
+    (pods * (current_value / desired_value)).ceil
+  end
 end
 
-if new_pods != pods
-  puts "scaling #{direction}"
-  command = %(kubectl scale --replicas #{new_pods} #{resource} -l #{selector})
-  puts command
-  puts %x(#{command})
-  `curl -X POST https://katana.wishpond.com/notify/scale \
-        --header "Content-Type: application/json" \
-        --header "X-KATANA-TOKEN:#{ENV['KATANA_SECRET']}" \
-        -d "{\"name\":\"#{name}\",\"selector\":\"#{selector}\",\"cpu\":\"#{cpu}\",\"pods\":\"#{pods}\",\"new_pods\":\"#{new_pods}\",\"utilisation\":\"#{utilisation}\",\"resource\":\"#{resource}\",\"direction\":\"#{direction}\"}"`
-        #,\"scale_up\":\"$scale_up\",\"scale_down\":\"$scale_down\",\"factor\":\"$factor\"
-end
+Autoscaler.run
